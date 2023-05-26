b5099bc313fecc011b544de6cc7769bcd6ee966d
DannyThomas1 <danny.thomas@bell.net>
2023-05-26 14:22:27 -0400

    rmv cache setting on api

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -50,7 +50,7 @@ const Feed = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch('api/prompt', { cache: 'no-store' });
+      const response = await fetch('api/prompt');
       const data = await response.json();
 
       setPosts(data);
